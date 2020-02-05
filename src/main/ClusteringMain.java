ebd83485044aec7782cc0fbaddc2e04add4944fd
Sraq-Zit <34834428+Sraq-Zit@users.noreply.github.com>
2020-02-05 20:22:12 +0100

    Add files via upload

--- src/main/ClusteringMain.java
+++ src/main/ClusteringMain.java
@@ -6,15 +6,18 @@ import mlearning.*;
 
 
 public class ClusteringMain {
-	public static Clustering.Algorithm ALGO = Clustering.Algorithm.KMEANS;
 	public static void main(String[] args) throws IOException {
 		Map<String, String> data = new Corpus().getTestData(100, 0);
 		Set<Document> docs = new HashSet<>();
 		data.forEach((text, c) -> docs.add(new Document(Arrays.asList(text.split(" ")), c)));
 		System.out.println("Clustering ...");
-		
-		Clustering clustering = new Clustering(ALGO, docs, 20);
+		System.out.println("Kmean:");
+		Kmeans clustering = new Kmeans(docs, 20);
 		clustering.getClusters().forEach((c, d) -> System.out.println(c + ": " + d));
+		System.out.println();
+		System.out.println("Hierarchical:");
+		Hierarchical clustering2 = new Hierarchical(docs, 20);
+		clustering2.getClusters().forEach((c, d) -> System.out.println(c + ": " + d));
 		
 	}
 
